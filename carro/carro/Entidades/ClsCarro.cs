93740f5d4cd0dbf26ea25bf9f1b37f86e94268ca
William Diaz <williamefrain1994@gmail.com>
2021-07-28 13:41:28 -0600

    CLASE 02

---
+++ carro/carro/Entidades/ClsCarro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carro.Entidades
+{
+    class ClsCarro
+    {
+        private String color  ; 
+        private String marca;
+        private String modelo;
+        private int peso ;
+        private int capacidad;
+
+        public string Color { get => color; set => color = value; }
+        public string Marca { get => marca; set => marca = value; }
+        public string Modelo { get => modelo; set => modelo = value; }
+        public int Peso { get => peso; set => peso = value; }
+        public int Capacidad { get => capacidad; set => capacidad = value; }
+    }
+}
---
+++ carro/carro/Program.cs
@@ -0,0 +1,19 @@
+﻿using carro.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        
+
+        }
+    }
+}
